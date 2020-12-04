6090363e670da3d3c8d93797ff4161f9ef72b0cf
JackyW29 <jacky.wang23@ycdsbk12.ca>
2020-12-04 00:19:25 +0000

    completed question 1

--- f_to_c.py
+++ f_to_c.py
@@ -1,1 +1,10 @@
 
+# get degree measure in Fahrenheit
+measure_in_Fahrenheit = float(input("Enter degree measure in Fahrenheit: "))
+
+# compute the degree measure in Fahrenheit to celsius degree measure.
+Fahrenheit_in_celsius = 33.8
+calculated_measure_in_celsius = (measure_in_Fahrenheit * Fahrenheit_in_celsius)
+
+# output the calculated degree measure in celsius to the UserWarning
+print("The result in celsius degree measure is "+ str(calculated_measure_in_celsius))
